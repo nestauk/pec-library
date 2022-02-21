9915351bd7cbcca23f5df677de4fd157a4710144
India Kerle <india.kerle@nesta.org.uk>
2022-02-21 17:27:07 +0000

    refactored build_network

--- pec_library/getters/data_getters.py
+++ pec_library/getters/data_getters.py
@@ -1,7 +1,6 @@
 """
 Functions to call library API, loading and saving to s3. 
 """
-
 ####
 import requests
 import math
@@ -11,18 +10,21 @@ import gzip
 import os
 import logging
 import boto3
-
+import random
 import pandas as pd
 from fnmatch import fnmatch
 from typing import List
-
+import pandas as pd
+from fnmatch import fnmatch
+from typing import List
 ####
+
+s3 = boto3.resource("s3")
 
 logger = logging.getLogger(__name__)
 
 BASE_REQUEST = "https://discover.libraryhub.jisc.ac.uk/search?subject="
 JSON_FORMAT = "&format=json&page="
-
 
 def get_library_data(keyword: str) -> List:
     """
@@ -63,14 +65,12 @@ def get_library_data(keyword: str) -> List:
             else:
                 logger.warning(f"{responses.status_code} response code.")
 
+        for book in lib_data:
+            book['bibliographic_data']['keyword'] = keyword.replace('*', '')
+
         return lib_data
     else:
         logger.info(f"no results found for {keyword} keyword.")
-
-
-def get_s3_resource():
-    s3 = boto3.resource("s3")
-    return s3
 
 
 def get_s3_data_paths(s3, bucket_name, root, file_types=["*.jsonl"]):
@@ -95,21 +95,15 @@ def get_s3_data_paths(s3, bucket_name, root, file_types=["*.jsonl"]):
 
     return s3_keys
 
-
 def save_to_s3(s3, bucket_name, output_var, output_file_dir):
 
     obj = s3.Object(bucket_name, output_file_dir)
 
     if fnmatch(output_file_dir, "*.pkl") or fnmatch(output_file_dir, "*.pickle"):
         byte_obj = pickle.dumps(output_var)
-    elif fnmatch(output_file_dir, "*.gz"):
-        byte_obj = gzip.compress(json.dumps(output_var).encode())
-    else:
-        byte_obj = json.dumps(output_var)
     obj.put(Body=byte_obj)
 
     logger.info(f"Saved to s3://{bucket_name} + {output_file_dir} ...")
-
 
 def load_s3_data(s3, bucket_name, file_name):
     """
---
+++ pec_library/pipeline/build_network_utils.py
@@ -0,0 +1,155 @@
+"""
+Util functions to help build library network and add relevant node and edge attributes. 
+"""
+####
+import re
+from string import digits
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from pattern.text.en import singularize
+import networkx as nx
+import itertools
+from collections import Counter
+from datetime import datetime
+
+from typing import List
+from pec_library.getters.data_getters import get_library_data
+####
+
+KEYWORDS = ["heat pump*", "solar panel*", "home retrofit*", "decarbonisation", "solar pv"]
+
+def get_all_library_data(keywords: List) -> List:
+    """
+    Wrapper function to query Libraryhub's API based on a list of keywords.  
+
+    Input:
+        keyword (list): list of query terms used to query Libraryhub's API.
+
+    Output:
+        all_library_data (list of dicts): query results where each element 
+        of the list is a dictionary with 
+        data on bibliographic data, holdings and uri. 
+    """
+    all_library_data = get_library_data(keywords[0])
+
+    for keyword in keywords[1:len(keywords)]:
+        try:
+            all_library_data.extend(get_library_data(keyword))
+        except TypeError:
+            pass
+
+    return all_library_data
+
+def extract_publication_year(all_library_data: List) -> List:
+    """
+    Regex extract publication year from publication details field. 
+
+    Input:
+        all_library_data (list): query results where each element 
+        of the list is a dictionary with 
+        data on bibliographic data, holdings and uri. 
+
+    Output:
+        all_library_data (list of dicts): query results where each element 
+        of the list is a dictionary with 
+        data on bibliographic data incl. publication year, holdings
+        and uri. 
+    """
+
+    for book in all_library_data:
+        if "publication_details" in book["bibliographic_data"].keys():
+            book_pub_detail = book["bibliographic_data"]["publication_details"][0]
+            potential_years = re.findall(r"\d{4}", book_pub_detail)
+            if potential_years != []:
+                potential_years = potential_years[0]
+                if potential_years.startswith("18") or potential_years.startswith("19") or potential_years.startswith("2"):
+                    potential_years_datetime_format = datetime.strptime(
+                        potential_years, "%Y"
+                    )
+                    if "subject" in book["bibliographic_data"]:
+                        book["bibliographic_data"][
+                            "publication_year"
+                        ] = potential_years_datetime_format.year
+        
+    return all_library_data
+
+def clean_subject(subject: List) -> List:
+    """
+    Args:
+        subject (list): list of string subjects to be cleaned.
+    
+    Returns:
+        subject (list): list of cleaned string subjects.
+    """
+
+    lemmatizer = WordNetLemmatizer()
+    all_clean_subs = []
+
+    subject = [sub.translate(str.maketrans("", "", ".:()-*")) for sub in subject]
+        # remove stopwords
+    subject = [
+        sub.lower() for sub in subject if sub not in stopwords.words("english")
+    ]
+    # remove digits
+    no_digits = str.maketrans("", "", digits)
+    subject = [sub.translate(no_digits) for sub in subject]
+    # trim trailing whitespace
+    subject = [re.sub("\s+", " ", sub).strip() for sub in subject]
+    # lemmatize subject
+    subject = [lemmatizer.lemmatize(sub) for sub in subject]
+    # trim short subjects
+    subject = [sub for sub in subject if len(sub) > 3]
+    # singularise subject
+    subject = [singularize(sub) for sub in subject]
+
+    return subject
+
+def build_subject_pair_coo_graph(all_library_data:List, min_edge_weight):
+    """
+    Builds subject pair cooccurance graph from records with both
+    publicaion year and subject list associated to them. 
+
+    Input:
+        all_library_data (list): query results where each element 
+        of the list is a dictionary with 
+        data on bibliographic data incl. publication year, holdings and uri. 
+
+    Output:
+        G (graph): An undirected, unweighted networkx graph where each
+        node is a subject, each edge contains year first published attribute.
+    """
+
+    
+    #filter records for records w/ subject AND publication year
+    subjects = [book['bibliographic_data']['subject'] for book in all_library_data if 'subject' and 'publication_year' in book["bibliographic_data"].keys()]
+
+    # Get all of the unique subjects 
+    subjects = tuple(sorted(set(itertools.chain(*subjects))))
+
+    # Get a list of all of the combinations you have
+    expanded_subjects = itertools.chain(*[tuple(itertools.combinations(d, 2)) for d in subjects])
+
+    # Sort and count the combinations so that A,B and B,A are treated the same
+    weighted_expanded_subjects = Counter([tuple(sorted(d)) for d in expanded_subjects])
+
+    #remove pairs with edgeweight 1
+    weighted_expanded_subjects = Counter(subject_pair for subject_pair in weighted_expanded_subjects.elements() if weighted_expanded_subjects[subject_pair] > min_edge_weight)
+    
+    #add time attribute to subject pairs 
+    subject_pair_years = {}
+    for subject_pair in weighted_expanded_subjects:
+        years = []
+        for record in all_library_data:
+        	if 'subject' in record["bibliographic_data"].keys():
+	            if subject_pair[0] and subject_pair[1] in record["bibliographic_data"]["subject"]:
+	                years.append(record["bibliographic_data"]["publication_year"])
+	                subject_pair_years[subject_pair] = years
+	    
+    #instantiate and populate network
+    G = nx.Graph()
+    for subject_pair, year in subject_pair_years.items():
+        G.add_edge(subject_pair[0], 
+                   subject_pair[1],
+                   first_published = sorted(year)[0])
+	
+    return G
